ef1bb0d0d8390a399297410b260f1fc5215ff465
oscarfhdev <oscarfh.dev@gmail.com>
2026-01-28 23:55:24 +0100

    initalized project

---
+++ 2do Trimestre/GestionCine/src/main/java/com/ofhcinema/GestionCine/GestionCineApplication.java
@@ -0,0 +1,13 @@
+package com.ofhcinema.GestionCine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCineApplication.class, args);
+	}
+
+}
---
+++ 2do Trimestre/GestionCine/src/test/java/com/ofhcinema/GestionCine/GestionCineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofhcinema.GestionCine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
